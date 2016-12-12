0fa4acb08f384411d3fdaff892b3857aa4330c5d
aequanimitas <htavenido@gmail.com>
2016-12-12 14:53:05 +0800

    mock, controllers and reward model

--- web/controllers/reward_controller.ex
+++ web/controllers/reward_controller.ex
@@ -2,6 +2,11 @@ defmodule Prizex.RewardController do
   use Prizex.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", rewards: Repo.all Prizex.Reward
+  end
+
+  def show(conn, %{"id" => id}) do
+    reward = Repo.get Prizex.Reward, id
+    render conn, "show.html", reward: reward
   end
 end
---
+++ web/models/reward.ex
@@ -0,0 +1,4 @@
+defmodule Prizex.Reward do
+  # time required defaults to minutes
+  defstruct [:name, :id, :time_required]
+end
